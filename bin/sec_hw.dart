c22088bba366460d8de966b31870653e54e7a9c9
Lamis Abdullah <lamisabdullah@outlook.com>
2022-05-18 23:58:07 +0300

    Initial commit

---
+++ bin/sec_hw.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  student.remove(null);
+  print(student);
+}
+
+List<String?> student = ['Ahmed', null, 'Abdullah'];
---
+++ test/sec_hw_test.dart
@@ -0,0 +1,8 @@
+import 'package:sec_hw/sec_hw.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
